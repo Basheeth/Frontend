615be1fea65b2695297a03c598bd7b411d208cd0
Basheeth <120025901+Basheeth@users.noreply.github.com>
2022-12-07 18:32:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 
 
-function Person(fi,ls,fc,fn){ 
-    this.firstName = firstName;
-    this.lastNumber = lastNumber;
-    this.favoriteColor = favoriteColor;
-    this.favoriteNumber = favoriteNumber;
-    // this.multiplyFavoriteNumber =function(num){
-    //   return num*this.favoriteNumber;
-    // }
-  }
-  
-  Person.prototype.multiplyFavoriteNumber = function(){
-    return num*this.favoriteNumber;
-  }
-  
   Array.prototype.count = function(char){
   
       let count = 0;
@@ -30,10 +16,7 @@ function Person(fi,ls,fc,fn){
     }
     
   }
-  let sae = new Array();
-  sae = [2,3,4,5,2,3,2,2,2];
-  console.log(sae);
-  console.log(sae.count());
+
   
   
   String.prototype.count = function(char){
@@ -50,9 +33,5 @@ function Person(fi,ls,fc,fn){
       return count;
     }
   }
-  let sa =new String();
-  sa = "vijay Kannan";
+
   
-  
-  console.log(sa);
-  console.log(sa.count("n"));
